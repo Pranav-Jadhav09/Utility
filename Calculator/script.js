d224a4480e4ad0cf742278f1e45c2307e4a71e69
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-15 10:50:23 +0530

    refact: ♻️  dynamic btns

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,24 +1,71 @@
-("use strict");
-
 const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
+const buttonsContainer = document.querySelector(".buttons");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
-// Function to calculate based on button clicked
+// Define an array of button values including numbers and operators
+const buttonValues = [
+  "AC",
+  "DEL",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  "00",
+  ".",
+  "=",
+];
+
+// Function to create a button element with a given value
+const createButton = (value) => {
+  const button = document.createElement("button");
+  button.textContent = value;
+  button.dataset.value = value;
+  button.classList.add("btn");
+  if (specialChars.includes(value) || value === "0") {
+    button.classList.add("operator");
+  }
+  return button;
+};
+
+// Function to render buttons
+const renderButtons = () => {
+  buttonValues.forEach((value) => {
+    const button = createButton(value);
+    buttonsContainer.appendChild(button);
+  });
+};
+
+// Call the renderButtons function to generate buttons on page load
+renderButtons();
+
+// Define function to calculate based on button clicked
 const calculate = (btnValue) => {
   display.focus();
 
   if (btnValue === "=" && output !== "") {
-    // If output has '%', replace with '/100' before evaluating
-    output = eval(output.replace("%", "/100"));
+    // Evaluate the expression
+    let result = eval(output.replace("%", "/100"));
+
+    // Round the result to 10 decimal places (adjust as needed)
+    result = Number(result.toFixed(10));
+    output = result.toString();
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    // If DEL button is clicked, remove the last character from the output
     output = output.toString().slice(0, -1);
   } else {
-    // If output is empty and button is specialChars then return
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
@@ -26,8 +73,10 @@ const calculate = (btnValue) => {
   display.value = output;
 };
 
-// EventListener to buttons, call calculate() on click
-buttons.forEach((button) => {
-  // Button click listener calls calculate() with dataset value as argument.
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+// Add event listener to buttons, call calculate() on click
+buttonsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    // Button click listener calls calculate() with dataset value as argument.
+    calculate(e.target.dataset.value);
+  }
 });

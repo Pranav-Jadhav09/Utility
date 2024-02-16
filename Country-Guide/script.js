30b4189abd9164272afedc91fd54331b0b04fc3f
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-16 11:21:11 +0530

    added new game 🎮

---
+++ Country-Guide/script.js
@@ -0,0 +1,70 @@
+("use strict");
+
+const searchBtn = document.getElementById("search-btn");
+const input = document.getElementById("country-input");
+const result = document.getElementById("result");
+
+searchBtn.addEventListener("click", () => {
+  let countryName = input.value;
+  let URL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      result.innerHTML = `
+      <img src="${data[0].flags.svg}" class="flag-img">
+
+      <h2>${data[0].name.common}</h2>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Capital:</h4>
+          <span>${data[0].capital[0]}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Continent:</h4>
+          <span>${data[0].continents[0]}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Population:</h4>
+          <span>${data[0].population}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Currency:</h4>
+          <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${
+        Object.keys(data[0].currencies)[0]
+      } 
+          </span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Common Languages:</h4>
+          <span>${Object.values(data[0].languages)
+            .toString()
+            .split(",")
+            .join(", ")}</span>
+        </div>
+      </div>
+    
+
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty.</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});

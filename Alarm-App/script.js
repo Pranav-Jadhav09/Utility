1cde34813fdf8ae52cc9fae7a71305c06a5e89ba
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-28 11:12:38 +0530

    🧠 added

--- Alarm-App/script.js
+++ Alarm-App/script.js
@@ -1,1 +1,97 @@
 ("use strict");
+
+// Selecting Elements
+const currentTime = document.querySelector("h1");
+const content = document.querySelector(".content");
+const selectMenu = document.querySelectorAll("select");
+const setAlarmBtn = document.querySelector("button");
+
+// Required Variables
+let alarmTime, isAlarmSet;
+const ringtone = new Audio("./assets/ringtone.mp3");
+
+/**
+ * Generating Hours
+ */
+for (let i = 12; i > 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+
+  let option = `<option value="${i}">${i}</option>`;
+  selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+/**
+ * Generating Minutes
+ */
+for (let i = 59; i >= 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+
+  let option = `<option value="${i}">${i}</option>`;
+  selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+/**
+ * Generating AM/PM
+ */
+for (let i = 2; i >= 0; i--) {
+  let ampm = i == 1 ? "AM" : "PM";
+
+  let option = `<option value="${ampm}">${ampm}</option>`;
+  selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+setInterval(() => {
+  let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM";
+
+  if (h >= 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h == 0 ? (h = 12) : h;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+  if (alarmTime === `${h}:${m} ${ampm}`) {
+    ringtone.play();
+    ringtone.loop = true;
+  }
+});
+
+/**
+ * Function to set alarm
+ */
+function setAlarm() {
+  if (isAlarmSet) {
+    alarmTime = "";
+    ringtone.pause();
+    content.classList.remove("disable");
+    setAlarmBtn.innerText = "Set Alarm";
+    return (isAlarmSet = false);
+  }
+
+  let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+
+  // Error Handling
+  if (
+    time.includes("Hour") ||
+    time.includes("Minute") ||
+    time.includes("AM/PM")
+  ) {
+    return alert("Please, select a valid time to set Alarm!");
+  }
+  alarmTime = time;
+  isAlarmSet = true;
+  content.classList.add("disable");
+  setAlarmBtn.innerText = "Clear Alarm";
+}
+
+// EventListener on Button
+setAlarmBtn.addEventListener("click", setAlarm);

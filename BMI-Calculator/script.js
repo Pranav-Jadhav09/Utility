895ab17be9fe87ebdd5e398a8263d8e4a4297e0b
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-29 20:26:08 +0530

    🧠 merged

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -1,1 +1,39 @@
 ("use strict");
+
+/**
+ * Calculate The BMI
+ */
+function calculate() {
+  let BMI;
+  const result = document.getElementById("result");
+
+  // Weight
+  const weight = parseInt(document.getElementById("weight").value);
+  document.getElementById("weight-val").textContent = weight + "kg";
+
+  // Height
+  const height = parseInt(document.getElementById("height").value);
+  document.getElementById("height-val").textContent = height + "cm";
+
+  // Formula
+  BMI = (weight / Math.pow(height / 100, 2)).toFixed(1);
+  result.textContent = BMI;
+
+  // Color Change on BMI value
+  if (BMI < 28.5) {
+    category = "Underweight";
+    result.style.color = "#ffc44d";
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    category = "Normal Weight";
+    result.style.color = "#0be881";
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    category = "Overweight";
+    result.style.color = "#ff884d";
+  } else {
+    category = "Obese";
+    result.style.color = "#ff5e57";
+  }
+
+  // Category Output
+  document.getElementById("category").textContent = category;
+}
